40b9ae03fea2c1b53b56fa8a9a32f8bea506ef1e
DickinsonMark <monoposon@comcast.net>
2016-10-29 16:58:08 -0600

    wired up html page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// jquery test
-$(document).on('ready', () => {
-  console.log('sanity check!');
+$(document).ready(function() {
+
 });
-
-// es6 test
-const PI = 3.14;
-
-// mocha test
-function sum(num1, num2) {
-  return parseInt(num1) + parseInt(num2);
-}
