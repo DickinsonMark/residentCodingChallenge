68f3e88507353ee3eb901a6ba181f7b81399fa55
DickinsonMark <monoposon@comcast.net>
2016-10-30 10:36:06 -0600

    destructured the input files successfully int a recursive function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log('sanity check');
 
   $('#fileUpload').on('submit', function(e) {
     e.preventDefault();
@@ -14,11 +13,22 @@ $(document).ready(function() {
     function onloadendFn() {
       const result = JSON.parse(fr.result);
       for (let i = 0; i < result.length; i++) {
-        if (Array.isArray(result[i].content)) {
-          console.log(result);
+        changeToHTML(result[i]);
+      }
+
+      function changeToHTML(input) {
+        if (Array.isArray(input.content)) {
+          for (let j = 0; j < input.content.length; j++) {
+            console.log('array', input.content[j]);
+            changeToHTML(input.content[j])
+          }
+        } else if (Object(input.content) === input.content) {
+          console.log('object', input.content.tag);
+          changeToHTML(input.content)
+        } else {
+          console.log('string', input.content);
         }
       }
-      console.log(result[0]);
     }
   });
 });
