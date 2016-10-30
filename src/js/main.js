6a3990fc570ec93cc9688ea96a68e540e77819f7
DickinsonMark <monoposon@comcast.net>
2016-10-29 19:17:35 -0600

    pulling data in from file using eval, will try to refactor after finishing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
   console.log('sanity check');
+
+  $('#fileUpload').on('submit', function(e) {
+    e.preventDefault();
+    // get the file input in jQuery
+    const $inputFile = $('#file');
+    const $input = $inputFile[0];
+    const $file = $input.files[0];
+    const fr = new FileReader();
+    console.log(fr);
+    fr.onloadend = function() {
+      $('.output').text(fr.result);
+      console.log(fr.result);
+      const result = eval(fr.result);
+
+      for (let i = 0; i < result.length; i++;){
+        
+        if (result.tag && Array.isArray(result.content)) {
+
+        }
+      }
+      console.log(result[0]);
+    };
+    fr.readAsText($file);
+  });
 });
