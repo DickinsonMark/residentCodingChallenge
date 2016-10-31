25746e9553340cfc8586845868b55b31d94eca47
DickinsonMark <monoposon@comcast.net>
2016-10-30 18:21:17 -0600

    commented the code to explain steps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Resident Coding Challenge
+
+For the code challenge, you will be building a simple web page that will allow the user to load a .json file that follows a certain syntax but is arbitrarily nested. When that file is loaded, you will generate HTML based on the nested structure and display that HTML on the page.
--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,53 @@
 $(document).ready(function() {
+  'use strict';
 
   $('#fileUpload').on('submit', function(e) {
+    //capture the film submission and prevent default behavior
     e.preventDefault();
+
     // get the file input in jQuery
     const $inputFile = $('#file');
     const $input = $inputFile[0];
     const $file = $input.files[0];
-    const fr = new FileReader();
-    fr.onloadend = onloadendFn;
+    const fileRead = new FileReader();
+    fileRead.onloadend = onloadendFn;
 
+    // function to call when the file is done loading
     function onloadendFn() {
-      const result = JSON.parse(fr.result);
+      // parse the files contents into JSON format
+      const result = JSON.parse(fileRead.result);
       const $output = $('#output');
 
+      // remove the current contents of the output display
       $output.empty();
       for (let i = 0; i < result.length; i++) {
+        //loop through the results of th file read
         var rootElement = $(`<${result[i].tag}></${result[i].tag}>`);
         $output.append(rootElement);
+        // call the function that does the actual work of the program!
         changeToHTML(result[i].content, rootElement);
       }
 
       function changeToHTML(input, parent) {
+        // check what the type of input is
         if (Array.isArray(input)) {
+          // if it's an array then loop through the inputs and recall the function on each
           for (let j = 0; j < input.length; j++) {
             changeToHTML(input[j], parent);
           }
         } else if (Object(input) === input) {
+          // if it's an object take the input's tag key and create the newparent for the function recall
           const newParent = $(`<${input.tag}></${input.tag}`);
           parent.append(newParent);
+          // recall the function with the content of the input as the new input
           changeToHTML(input.content, newParent);
         } else {
+          // if the input is a string then add that as the content to the parent element
           $(parent).html(input);
         }
       }
     }
-    fr.readAsText($file);
+    // read the file and output a text string
+    fileRead.readAsText($file);
   });
 });
